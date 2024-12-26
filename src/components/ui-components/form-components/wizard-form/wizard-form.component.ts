afed84cc6a1d995f6f58e16bf7ef459bf8d51e9a
Akash Chandrakant Sontakke <sontakke.tech@gmail.com>
2024-12-26 09:43:42 +0530

    added form components

---
+++ src/components/ui-components/form-components/wizard-form/wizard-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-wizard-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './wizard-form.component.html',
+  styleUrl: './wizard-form.component.scss'
+})
+export class WizardFormComponent {
+
+}
